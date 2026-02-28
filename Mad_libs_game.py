9c2f9c67c008e6a6c660747ca9241fccf9b0140a
AitmadAhmed <148468315+AitmadAH@users.noreply.github.com>
2026-02-28 23:05:42 +0500

    Add files via upload
    Mad libs I seen first time and I created this. It is funny game I like it.

---
+++ Mad_libs_game.py
@@ -0,0 +1,25 @@
+print("Mad libs game by junior python developer AITMAD")
+
+room = input("Enter a room:  ")
+place = input("Enter a place:  ")
+adverb = input("Enter an adverb:  ")
+food = input("Enter a food:  ")
+ptverb = input("Enter a past tense verb:  ")
+sfood = input("Enter same food:  ")
+ptverb2 = input("Enter another past tense verb:  ")
+sfood2 = input("Enter same food:  ")
+country = input("Enter a country:  ")
+fomeat = input("Enter a type of meat:  ")
+veggie = input("Enter a vegetable:  ")
+spice = input("Enter a spice:  ")
+number = input("Enter a number:  ")
+ptverb3 = input("Enter another past tense verb:  ")
+adjective = input("Enter an adjective:  ")
+ptverb4 = input("Enter another past tense verb:  ")
+emuwords = input("Enter made up word:  ")
+
+
+
+print(f"Charlie and Itchy were in the {room} of {place}. They were having a {adverb} competition. A cook off! Sasha was the judge! “Ok, you will both be making {sfood} with cheese!” Charlie and Itchy both got to work! Charlie {ptverb} a pan and began to make the {sfood2}. Itchy {ptverb2} a pot and started to make the {sfood2}. Charlie and Itchy started by adding {country} {fomeat}, some {veggie}, and cheese! Itchy added some {spice}. After {number} hour (s), the time was up! Sasha tried Charlie’s dish first, she {ptverb3} and said it was TAWDRY. Then she tried Itchy’s dish. She {ptverb4} with a smile! “HANDS Itchy! You win” she Said")
+
+
